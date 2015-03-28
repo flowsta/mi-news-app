940a335320a63d220d04597b3e57c9bc9e186eee
flowsta <adolflow@gmail.com>
2015-03-28 11:36:23 +0100

    Comienza la fiesta

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+csv_path = './static/espanaenllamas.csv'
+csv_obj = csv.DictReader(open(csv_path, 'r'))
+csv_list = list(csv_obj)
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+        object_list=csv_list,
+    )
 
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
-	port=8000,
-	use_reloader=True,
-	debug=True,
+        port=8000,
+        use_reloader=True,
+        debug=True,
     )
-
+    
     
 
